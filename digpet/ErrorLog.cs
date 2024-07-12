d9bbe46f2e0f2490cf18c0bf65b90dd4bf1814fb
tama_RFT <kitgareth4@gmail.com>
2024-07-12 20:15:47 +0900

    事前にあるソースファイルなどを追加

---
+++ digpet/ErrorLog.cs
@@ -0,0 +1,21 @@
+﻿namespace digpet
+{
+    static class ErrorLog
+    {
+        public static void ErrorOutput(string name, string msg, bool show)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("errorLog.txt", true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss [") + name + "]" + msg + "\r\n");
+                }
+                if (show) MessageBox.Show(msg, name, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
