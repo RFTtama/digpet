836fd368074f512ff8fb540c90ffa60c67d5b2e6
tama_RFT <kitgareth4@gmail.com>
2024-07-21 18:04:30 +0900

    バンドルされない例外がスローされたときにプログラム側でキャッチするように変更

--- digpet/Program.cs
+++ digpet/Program.cs
@@ -10,8 +10,15 @@ namespace digpet
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            try
+            {
+                ApplicationConfiguration.Initialize();
+                Application.Run(new Form1());
+            }
+            catch (Exception ex)
+            {
+                ErrorLog.ErrorOutput("ñ¢íËã`ÇÃÉGÉâÅ[", ex.Message, true);
+            }
         }
     }
 }
