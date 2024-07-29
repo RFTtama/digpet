b11a30429fefbd64b8ef10e018af6605cbf9bf15
tama_RFT <kitgareth4@gmail.com>
2024-07-29 20:34:37 +0900

    アプリを重複起動した際にエラーが出力されるように(debugとreleaseのすみ分け済み)

--- digpet/Program.cs
+++ digpet/Program.cs
@@ -10,14 +10,53 @@ namespace digpet
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+#if !DEBUG
+            Mutex mutex = new Mutex(false, "digpet");                           //Mutex(Release用)
+#endif
+#if DEBUG
+            Mutex mutex = new Mutex(false, "digpet_debug");                     //Mutex(Debug用)
+# endif
+
+            bool hasHandle = false;                                             //ハンドルされているか
+
             try
             {
+                try
+                {
+                    hasHandle = mutex.WaitOne(0, false);                        //Mutexがすでにハンドルされているか確かめる
+                }
+                catch (System.Threading.AbandonedMutexException)                //ハンドルされていない
+                {
+                    hasHandle = true;                                           //このアプリでハンドルする
+                }
+                if (hasHandle == false)                                         //このアプリでハンドルしない
+                {
+                    MessageBox.Show("Digpetは既に実行されています",           //エラー
+                        "Digpet実行エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
                 ApplicationConfiguration.Initialize();
                 Application.Run(new Form1());
             }
             catch (Exception ex)
             {
                 ErrorLog.ErrorOutput("未定義のエラー", ex.Message, true);
+
+                if (hasHandle)                                                  //Mutexの解放
+                {
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
+            }
+            finally
+            {
+                if (hasHandle)                                                  ////Mutexの解放
+                {
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
             }
         }
     }
