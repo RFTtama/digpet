0da93c34051975bdcf12d3b03fbd59daaaff1f65
tama_RFT <kitgareth4@gmail.com>
2025-02-01 21:22:22 +0900

    ErrorLogなどをタスクとして表示するように

---
+++ digpet/Modules/TaskMessager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digpet.Modules
+{
+    public static class TaskMessager
+    {
+        public static void OutputMessage(string message, string name)
+        {
+            Task.Run(() =>
+            {
+                OutputMessage(message, name, MessageBoxButtons.OK, MessageBoxIcon.None);
+            });
+        }
+
+        public static void OutputMessage(string message, string name, MessageBoxButtons button, MessageBoxIcon icon)
+        {
+            Task.Run(() =>
+            {
+                MessageBox.Show(message, name, button, icon);
+            });
+        }
+    }
+}
