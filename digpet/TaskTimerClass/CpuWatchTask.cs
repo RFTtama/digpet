8620accb1a3e1fd49b6d64629ed2f431b5ddf7ea
tama_RFT <kitgareth4@gmail.com>
2025-03-12 22:49:33 +0900

    ひな型を作成

---
+++ digpet/TaskTimerClass/CpuWatchTask.cs
@@ -0,0 +1,26 @@
+﻿using digpet.Models.AbstractModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digpet.TaskTimerClass
+{
+    class CpuWatchTask : TASK
+    {
+        public CpuWatchTask(int interval, int waitTime) : base(interval, waitTime)
+        {
+        }
+
+        protected override TaskReturn Main()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Terminator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
