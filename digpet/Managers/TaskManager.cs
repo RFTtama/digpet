c1ed84e6daf0cf65b3e967611e67a244080b9d6c
tama_RFT <kitgareth4@gmail.com>
2025-03-24 20:57:58 +0900

    TASK管理用のクラスを追加

---
+++ digpet/Managers/TaskManager.cs
@@ -0,0 +1,18 @@
+﻿using digpet.Models.AbstractModels;
+
+namespace digpet.Managers
+{
+    internal class TaskManager
+    {
+        public readonly TASK[] Tasks;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="maxTaskNum">登録可能なタスクの最大数</param>
+        public TaskManager(int maxTaskNum)
+        {
+            Tasks = new TASK[maxTaskNum];
+        }
+    }
+}
